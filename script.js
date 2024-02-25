44611f722c38114338d6d73dc86843d7d8a747ce
olivia1117 <oadamic@gmail.com>
2024-02-25 10:21:38 -0600

    updaing css to be more responsive, making this work to call the RESTful API from node server

--- script.js
+++ script.js
@@ -5,19 +5,48 @@
 //Have meaningful field headers, all numbers right justified, and all random fields read only
 //Have all numbers right justified and all random number fields implemented as read only
 
-function generateRandomNumber() {
-    const randomNum = Math.floor(Math.random() * 6) + 1;
-    document.getElementById("randomNumberDisplay").textContent = randomNum; //set the text to be the random number
-    document.getElementById("generateButton").textContent = "Roll Again!"; //roll again button to generate a new number
-  }
+//this function was the original dice roller to generate and display the random num
+// function generateRandomNumber() {
+//     const randomNum = Math.floor(Math.random() * 6) + 1;
+//     document.getElementById("randomNumberDisplay").textContent = randomNum; //set the text to be the random number
+//     document.getElementById("generateButton").textContent = "Roll Again!"; //roll again button to generate a new number
+//     // document.getElementById("randomNumberDisplay").innerHTML = "9"; 
+//     // document.getElementById("die3").innerHTML = "13"; 
+//     // document.getElementById("die").value = "14"; 
+//   }
 
+//this function was from the original dice roller so that the random num would be 
+//displayed if the enter key was pressed 
   document.addEventListener("keypress", function(event) {
     if (event.key === "Enter") { // check if the Enter key is pressed
-      generateRandomNumber(); // call the function to generate the random number
+      displayNum(); // call the function to display the random number
     }
   });
 
-  generateRandomNumber();
+
+//this function will take the num from node and then display it to the screen 
+async function displayNum() {
+  const randNum = await fetchGenerateRandomNum(); 
+  document.getElementById("randomNumberDisplay").textContent = randNum;
+}
+
+//this function gets the random number from the node.js server
+async function fetchGenerateRandomNum() {
+  const url = "https://dice-roller-node.azurewebsites.net/generateRandomNum";
+  /*when the code below is uncommented, it will cause a CORS (cross origin resource sharing) error
+  because it is trying to find the page with that remote resource but it doesn't exist
+  becuase the address is incorrect. when the console is opened up on the dice roller page
+  with the inspect option, then this error will appear*/
+  // const url = "https://dice-roller.azurewebsites.net/generateRandomNum";
+  const response = await fetch(url); 
+  const responseText = await response.text(); 
+  return responseText; 
+
+
+}
+
+
+  
 
 
 
